e14c2261d23a17f798a0d350f0adf6a095ca8f8d
Aalekh-coder <dheetaalekh@gmail.com>
2025-08-07 17:39:36 +0530

    blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata:Metadata={
-    title:"All blog- Aalekh ",
+    title:"All blog - Aalekh",
     description:"all blog"
 }
 
